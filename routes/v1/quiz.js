bb81252e8600158cd861e3288f173a9c4ae03daf
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-20 11:02:21 +1200

    feat(delete quiz route): delete quiz route

--- routes/v1/quiz.js
+++ routes/v1/quiz.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
-import { createQuiz, getQuiz, participateQuiz } from "../../controllers/v1/quiz.js";
+import { createQuiz, getQuiz, participateQuiz, deleteQuiz } from "../../controllers/v1/quiz.js";
 
 router.route("/").get(getQuiz).post(createQuiz);
+router.route("/:id").delete(deleteQuiz)
 router.route("/:id/participate").post(participateQuiz);
 
 export default router;
