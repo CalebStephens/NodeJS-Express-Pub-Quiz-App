bc55c619bf9e4ffe6ef9f529c90b5a8a21773e30
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-19 14:06:53 +1200

    feat(caching, ratelimiting, compression,cors): caching, ratelimiting,
    compression,cora

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import express, { urlencoded, json } from "express";
+import rateLimit from 'express-rate-limit'
 import cors from "cors";
 import helmet from "helmet";
+import cacheRoute from "./middleware/cacheRoute.js";
+import compression from "compression";
 
 /**
  * You will create the routes for institutions and departments later
@@ -14,8 +17,6 @@ import categories from "./routes/v1/category.js";
 import quiz from "./routes/v1/quiz.js";
 
 dotenv.config();
-
-
 
 const app = express();
 
@@ -32,6 +33,16 @@ app.use(urlencoded({ extended: false }));
 app.use(json());
 app.use(cors());
 app.use(helmet());
+app.use(cacheRoute);
+app.use(compression());
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minute duration in milliseconds
+    max: 50,
+    message: 'You exceeded 50 requests in 1 min limit!',
+    headers: true,
+  })
+)
 
 app.use(`/${BASE_URL}/${CURRENT_VERSION}/auth`, auth);
 app.use(
